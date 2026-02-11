89159f676e348debbc1d7ef55284ada2cd6499fd
Kristjan Rang <mail@rang.ee>
2026-02-11 10:19:26 +0000

    feat: Add hook to access heavylog metadata before Karafka consumer runs

--- lib/heavylog/adapters/karafka_consumer.rb
+++ lib/heavylog/adapters/karafka_consumer.rb
@@ -7,7 +7,12 @@ module Heavylog
       def on_consume
         args = "topic=#{topic.name} partition=#{partition} messages=#{messages.count}"
         Heavylog.log_job(SecureRandom.uuid, "KarafkaLogger", self.class.name, args)
+        heavylog_before_consume
         super
+      end
+
+      def heavylog_before_consume
+        # This is a hook for the users to access heavylog metadata before the consumer runs.
       end
 
       def wrap(action)
